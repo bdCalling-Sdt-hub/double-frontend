4a2d80760a4093dfa27560e65dc784cf3455d025
apucsd <apusutradhar77@gmail.com>
2025-04-02 14:49:36 +0600

    added

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
         plugins: [react()],
         server: {
-                // host: '68.178.202.182',
-                host: '10.0.60.123',
-                port: 3006,
-                allowedHosts: ['apu.binarybards.online'], // Ensure this line is present
+                host: '68.178.202.182',
+                // host: '10.0.60.123',
+                // port: 3006,
+                // allowedHosts: ['apu.binarybards.online'], // Ensure this line is present
         },
-        // preview: {
-        //         allowedHosts: ['cannasseurshemp.com'],
-        // },
+        preview: {
+                allowedHosts: ['cannasseurshemp.com'],
+        },
 });
